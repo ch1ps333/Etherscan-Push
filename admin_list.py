13796ab7374c164fa8021a0dd3d0787b114d85a3
ch1ps333 <95697368+ch1ps333@users.noreply.github.com>
2024-11-11 18:25:24 +0200

    Update admin_list.py

--- admin_list.py
+++ admin_list.py
@@ -1,6 +1,8 @@
 from database import get_admins
+from asyncio import sleep, run as asrun
+
+import json
 from asyncio import sleep
-admin_list = []
 
 async def update_admin_list():
     global admin_list
@@ -8,4 +10,11 @@ async def update_admin_list():
         admins = await get_admins()
 
         admin_list = [admin.tg_id for admin in admins]
+        
+        with open('admin_list.json', 'w') as f:
+            json.dump(admin_list, f)
+
         await sleep(30)
+
+if __name__ == "__main__":
+    asrun(update_admin_list())
